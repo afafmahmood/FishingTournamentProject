282352b1fbf0d6f2f4cc361ceb4e0fd9bc8c339d
afaf mahmood <afafmahmood48@gmail.com>
2022-04-05 13:08:44 -0600

    Payment page

---
+++ Controllers/PaymentController.cs
@@ -0,0 +1,24 @@
+﻿using FishingTournament02.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FishingTournament02.Controllers
+{ 
+    public class PaymentController : Controller
+    {
+        private readonly ApplicationDbContext db;
+        public PaymentController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
