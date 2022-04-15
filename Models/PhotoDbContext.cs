b299d0c6794888f2d3ce4262e3ef88df2d6bf779
Rhonda Lammert <cookgirl60@yahoo.com>
2022-04-15 16:22:10 -0500

    m6

---
+++ Models/PhotoDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace FishingTournament02.Models
+{
+    public class PhotoDbContext : DbContext
+    {
+        public DbSet<Photo> Photos { get; set; }
+        public PhotoDbContext(
+            DbContextOptions<PhotoDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
+
