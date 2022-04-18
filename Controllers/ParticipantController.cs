22f503ad2eeb4e5fa50bf4e7a9cd259f1030b4f1
afaf mahmood <afafmahmood48@gmail.com>
2022-04-17 22:37:01 -0600

    Participant info added. Use Participant/AllParticipant Participant/AddParticipant Participant/EditParticipant

---
+++ Controllers/ParticipantController.cs
@@ -0,0 +1,44 @@
+﻿using FishingTournament02.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FishingTournament02.Controllers
+{
+    public class ParticipantController : Controller
+    {
+        ApplicationDbContext db;
+        public ParticipantController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        public IActionResult AllParticipant()
+        {
+            return View(db.Participants);
+        }
+        
+        //GET: /<controller>/
+        public IActionResult AddParticipant()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddParticipant(Participant participant)
+        {
+            db.Add(participant);
+            db.SaveChanges();
+            return RedirectToAction("AllParticipant");
+        }
+
+        public IActionResult EditParticipant(int id)
+        {
+            Participant participant;
+            participant = db.Participants.Find(id);
+            return View(participant);
+        }
+    }
+}
