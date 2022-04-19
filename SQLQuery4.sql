37d230654d30e9ddeae3fc7e479bad2e7256b003
afaf mahmood <afafmahmood48@gmail.com>
2022-04-19 12:38:46 -0600

    Minor syntax correction

---
+++ SQLQuery4.sql
@@ -0,0 +1,5 @@
+﻿SET IDENTITY_INSERT dbo.Participant ON;
+
+insert into Participant(participant_ID, participant_name, participant_phone, participant_event)
+
+values(101, 'Jenna', '7208867654', 'White Marlin')
