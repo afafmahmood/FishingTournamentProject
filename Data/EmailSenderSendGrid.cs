8de62245f398340329a5709060102a2cd17b9a72
jazmenb92 <jazmenb92@gmail.com>
2022-04-19 21:05:56 -0500

    the database forreal works this time.

---
+++ Data/EmailSenderSendGrid.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FishingTournament02.Data
+{
+    public class EmailSenderSendGrid
+    {
+    }
+}
