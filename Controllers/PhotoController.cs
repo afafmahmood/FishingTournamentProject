d69129a9f5ed3e8283859cc3152e702fd0aad35c
jazmenb92 <jazmenb92@gmail.com>
2022-04-28 23:46:07 -0500

    adding add profile to the admin index and fixed the text fields

--- Controllers/PhotoController.cs
+++ Controllers/PhotoController.cs
@@ -4,51 +4,39 @@ using System.IO;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using System.Collections.Generic;
-/*
+using System.Threading.Tasks;
 
 namespace FishingTournament02.Controllers
 {
     public class PhotoController : Controller
     {
-        private readonly IHostingEnvironment Environment;
-        public PhotoController(IHostingEnvironment _environment)
+        private readonly ApplicationDbContext db;
+        public PhotoController(ApplicationDbContext db)
         {
-            Environment = _environment;
+            this.db = db;
         }
         public IActionResult Index()
         {
             return View();
         }
-
-        [HttpPost]
-        public IActionResult Index(List<IFormFile> postedFiles)
+        public IActionResult AddPhoto()
         {
-            string wwwPath = this.Environment.WebRootPath;
-            string contentPath = this.Environment.ContentRootPath;
-
-            string path = Path.Combine(this.Environment.WebRootPath, "Uploads");
-            if (!Directory.Exists(path))
-            {
-            }
-            else
-            {
-                Directory.CreateDirectory(path);
-            }
-
-            List<string> uploadedFiles = new List<string>();
-            foreach (IFormFile postedFile in postedFiles)
-            {
-                string fileName = Path.GetFileName(postedFile.FileName);
-                using (FileStream stream = new FileStream(Path.Combine(path, fileName), FileMode.Create))
-                {
-                    postedFile.CopyTo(stream);
-                    uploadedFiles.Add(fileName);
-                    ViewBag.Message += string.Format("<b>{0}</b> uploaded.<br />", fileName);
-                }
-            }
-
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddPhoto(Photo p)
+        {
+            var path = Path.Combine(
+                Directory.GetCurrentDirectory(), "wwwroot\\images",
+                p.MyPhoto.FileName);
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                await p.MyPhoto.CopyToAsync(stream);
+            }
+            p.Url = p.MyPhoto.FileName;
+            db.Add(p);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
         }
     }
 }
-*/
